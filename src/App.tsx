f2f75286e393a5e6db402e4db54312df1fb9183c
원대호 <wondaeho@wondaehoui-MacBookAir.local>
2024-11-29 15:34:11 +0900

    수정 내용 설명

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 22
+          Learn React
         </a>
       </header>
     </div>
