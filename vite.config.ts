e25d002acb0a13b451fd18b8380de99eb9241993
wondaeho1 <wdh8366@gmail.com>
2024-12-04 16:52:13 +0900

    tailwinds, vite 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss'; //추가
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //css 추가
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
